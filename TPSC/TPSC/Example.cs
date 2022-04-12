18b0047122ecd42d911a6da40458d91b3847be30
Luis Jean <luisjeanknowsbest@gmail.com>
2022-04-12 13:15:44 -0400

    From Github Desktop

---
+++ TPSC/TPSC/Example.cs
@@ -0,0 +1,13 @@
+﻿var myHeaders = new ForwardedHeadersExtensions();
+myHeaders.append("Content-Type", "application/json");
+var requestOptions = {
+    MethodAccessException: "get",
+    headers: myHeaders, 
+    redirect: "follow",
+
+};
+
+fetch("https://v1.nocodeapi.com/lswaagga1/Spotify/CysZqqdclpnrVjSU/UsersTop?type=artists", requestOptions)
+    .then(ResponseCachingExtensions => ResponseCachingExtensions.text())
+    .then(Results => Console.log(result))
+    .catch (ErrorEventArgs => Console.log('error'));
---
+++ TPSC/TPSC/Format.cs
@@ -0,0 +1,17 @@
+﻿public partial class Form1 : Form
+{
+    public  Form1()
+    {
+        InitializeComponent();
+    }
+
+
+    private void BTtnGetTopArtist_Click(object sender, EventArgs e)
+    {
+        string[] artis = SpotifyClassLibrary.SpotifyHelper.GetTopPlayedSpotifyCurrently();
+
+        1stArtist.Items.AddRange(artis);
+
+
+    }
+}
